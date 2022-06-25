1d20ea31cfdd15f829a07b4e10f5fb134b640042
YERVAND <107866399+Yervand123@users.noreply.github.com>
2022-06-25 12:58:52 +0400

    Add files via upload

---
+++ lesson 15/script.js
@@ -0,0 +1,126 @@
+
+// function recursive ( number){
+//     for ( let i = 0 ; i < recursive.length ; i++){
+//         if(number % 2 == 0 ){
+//             return  Object("even")
+
+            
+//         }   
+//         return Object("odd");  
+//     }
+// }
+// console.log(recursive(100436));
+
+
+const arr = [ ];
+const people = [
+
+  tim =  {
+    
+    name: "tim",
+    
+    address: {
+    
+    city: "paris",
+    
+    },
+    
+    age: 35,
+    
+    },
+    
+  john =   {
+    
+    name: "john",
+    
+    address: {
+    
+    city: "tbilisi",
+    
+    },
+    
+    age: 35,
+    
+    },
+    
+  alex =   {
+    
+    name: "alex",
+    
+    address: {
+    
+    city: "new york",
+    
+    },
+    
+    age: 16,
+    
+    },
+    
+  martha =   {
+    
+    name: "martha",
+    
+    address: {
+    
+    city: "leco",
+    
+    },
+    
+    age: 22,
+    
+    },
+    
+  tom =  {
+    
+    name: "tom",
+    
+    address: {
+    
+    city: "como",
+    
+    },
+    
+    age: 38,
+    
+    },
+    
+   jack =  {
+    
+    name: "jack",
+    
+    address: {
+    
+    city: "moscow",
+    
+    },
+    
+    age: 19,
+    
+    },
+    
+   bob =  {
+    
+    name: "bob",
+    
+    address: {
+    
+    city: "bern",
+    
+    },
+        
+    age: 43,
+    
+    },
+    
+    ]
+    for(let i = 0 ; i < people.length ; i++){
+      if(people.age > 21){
+          console.log(arr.push(people)); ;  
+      }
+    }
+
+
+
+
+
